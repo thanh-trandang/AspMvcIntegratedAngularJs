536c0b2c00e43ec23b79e4aa6b214aa8830fb42a
thanhtda <trandangthanh@outlook.com>
2016-08-05 05:36:05 +0700

    refactored

--- 2. Presentation/Ebox.MvcIntegratedAngularJs/Scripts/App/EboxMain.js
+++ 2. Presentation/Ebox.MvcIntegratedAngularJs/Scripts/App/EboxMain.js
@@ -3,55 +3,7 @@
 })();
 
 
-(function () {
-    angular.module('EboxMain', ['Common']);
 
-    var BaseController = function ($scope, $http) {
-
-        var self = this;
-        self.SignInCommand = {
-            email: "trandangthanh@gmail.com",
-            password: "123456",
-            rememberMe: false,
-            debug: false
-        }
-
-        $scope.topic =
-           "Integrating ASP.NET MVC and AngularJS";
-        $scope.author = "Tran Dang Thanh";
-
-        this.search = {
-            keyword: "",
-            searchButtonLabel: "Search",
-            placeHolder: "Please enter your keyword..."
-        };
-
-
-
-        this.beginSearch = function () {
-            this.search.keyword = "Your search has been started. Please wait...";
-            $http.post('/Authentication/SignIn', self.SignInCommand)
-                .then(function successCallback(response) {
-                    // this callback will be called asynchronously
-                    // when the response is available
-                    // self.message = JSON.stringify(response);
-                }, function errorCallback(errorResponse) {
-                    // called asynchronously if an error occurs
-                    // or server returns response with an error status.
-                    self.message = JSON.stringify(errorResponse);
-                });
-        };
-
-        var initialize = function () {
-            $scope.pageHeading = "Home Section";
-        }
-
-        initialize();
-    }
-
-    BaseController.$inject = ['$scope', '$http'];
-    angular.module('EboxMain').controller("BaseController", BaseController);
-})();
 
 
 
---
+++ 2. Presentation/Ebox.MvcIntegratedAngularJs/Scripts/App/Home/App.js
@@ -0,0 +1,11 @@
+﻿
+(function () {
+    angular.module('HomeModule', ['Common'])
+        .config(function ($locationProvider) {
+            $locationProvider.html5Mode({
+                enabled: true,
+                requireBase: false
+            });
+        });   
+})();
+
