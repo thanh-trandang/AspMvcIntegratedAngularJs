341acf4821ebad99ae8042b277df8c93ae678446
thanhtda <trandangthanh@outlook.com>
2016-08-11 16:40:59 +0700

    add profile controller

---
+++ 2. Presentation/Ebox.MvcIntegratedAngularJs/Controllers/ProfileController.cs
@@ -0,0 +1,24 @@
+﻿using Ebox.MvcIntegratedAngularJs.Feature.UserProfile;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Ebox.MvcIntegratedAngularJs.Controllers
+{
+    public class ProfileController : Controller
+    {
+        // GET: Profile
+        public ActionResult EditProfile()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult EditProfile(EditProfileCommand editProfileCommand)
+        {
+            return View(editProfileCommand);
+        }
+    }
+}
---
+++ 2. Presentation/Ebox.MvcIntegratedAngularJs/Feature/UserProfile/EditProfileCommand.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Ebox.MvcIntegratedAngularJs.Feature.UserProfile
+{
+    public class EditProfileCommand
+    {
+        public String Email { get; set; }
+        public String FirstName { get; set; }
+        public String LastName { get; set; }
+        public DateTime DateOfBirth { get; set; }
+    }
+}
